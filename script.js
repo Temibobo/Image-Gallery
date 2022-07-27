98f34f26a4cdded37aa63bb239fd87db79f4de45
Temibobo <temitopeayannusi@gmail.com>
2022-07-27 15:21:19 +0100

    first commit in image gallery

---
+++ script.js
@@ -0,0 +1,56 @@
+const change = document.getElementById("cake 1");
+
+change.addEventListener("click", (event) => {
+     event.target.style.background = "red";
+     event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("cake 2");
+
+change2.addEventListener("click", (event) => {
+  event.target.style.background = "green";
+  event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("cake 3");
+
+change3.addEventListener("click", (event) => {
+  event.target.style.background = "blue";
+  event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("cake 4");
+
+change4.addEventListener("click", (event) => {
+  event.target.style.background = "brown";
+  event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("cake 5");
+
+change5.addEventListener("click", (event) => {
+  event.target.style.background = "pink";
+  event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("cake 6");
+
+change6.addEventListener("click", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("cake 7");
+
+change7.addEventListener("click", (event) => {
+  event.target.style.background = "yellow";
+  event.target.style.color = "black";
+});
+
+const change8 = document.getElementById("cake 8");
+
+change8.addEventListener("click", (event) => {
+  event.target.style.background = "purple";
+  event.target.style.color = "white";
+});
+
